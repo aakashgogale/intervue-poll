7067fdeedbf0602c1079fc4d324941671bc4df2c
Aakash Gogale <aakashgogale123@gmail.com>
2026-02-19 16:37:46 +0530

    Fresh upload of frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const pollRoutes = require("./routes/pollRoutes")
+const pollSocket = require("./sockets/pollSocket")
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://intervue-poll-umber.vercel.app", // Vercel frontend
+      "http://localhost:5173", // local dev (if needed)
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+
+
+app.use(express.json())
+
+app.use("/poll", pollRoutes)
+
+pollSocket(io)
+
+const PORT = process.env.PORT ||  3001
+server.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}`)
+})
